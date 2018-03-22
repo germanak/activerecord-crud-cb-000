54e3b0f2e58b734d91a80b50c955a49feb32f747
Alec German <alec_german@yahoo.com>
2018-03-22 22:11:43 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,6 +31,11 @@ def can_be_created_in_a_block(title: nil, release_date: nil)
       m.release_date = 1990
     end
   else
+    attributes = {
+      title: title
+      release_date: release_date
+    }
+    Movie.create(attributes)
   end
 end
 
