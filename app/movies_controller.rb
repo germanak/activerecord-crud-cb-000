4a2aa90507f8b866b08ff27325316335207f7245
Alec German <alec_german@yahoo.com>
2018-03-22 21:48:21 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,7 +45,7 @@ end
 def can_find_the_first_item_from_the_database_using_id
   sql = "SELECT * FROM movies WHERE id = ?"
 
-  ActiveRecord::Base.connection.execute(sql, @id)
+  ActiveRecord::Base.connection.execute(sql, @id).first.title
 end
 
 def can_find_by_multiple_attributes
