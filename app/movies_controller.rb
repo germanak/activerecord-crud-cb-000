b44aec48c87f0f9cac50be10c636af2e187cddbb
Alec German <alec_german@yahoo.com>
2018-03-22 21:50:41 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -43,9 +43,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  sql = "SELECT * FROM movies WHERE id = ?"
-
-  ActiveRecord::Base.connection.execute(sql, self.id).first
+  Movie.find_by(id:).title
 end
 
 def can_find_by_multiple_attributes
